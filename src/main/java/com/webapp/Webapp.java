9fe2ac5051508ee836352aa29a117ed9c40093d8
akikr <akikr@duck.com>
2024-05-26 17:22:40 +0530

    Add: The dependencies and update the webapp-service main class

--- src/main/java/com/webapp/Webapp.java
+++ src/main/java/com/webapp/Webapp.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebappDemoProjApplication {
+public class Webapp
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebappDemoProjApplication.class, args);
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Webapp.class, args);
 	}
 
 }
